36e2413f15e9922f5b6046d9e4cc422c0fc56230
Liebe Brand <liebebrand@gmail.com>
2018-08-22 15:13:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,23 @@
 # Your code goes here!
 
 # class Dog
-# 
+#
 #   def initialize(name) # initialize the method
 #     @name = name
 #   end
-# 
+#
 #   def name=(name) # writing the method
 #     @name = name
 #   end
-# 
+#
 #   def name # reading the method
 #     @name
 #   end
-# 
+#
 #   def bark
 # 		puts "woof!"
 # 	end
-# 
+#
 # end
 
 
@@ -25,12 +25,12 @@
 
 
 class Dog
-		attr_accessor( :name) # There are only methods on objects. 
-	# attr_accessor :name is equivalent to the code below 
+		attr_accessor( :name) # There are only methods on objects.
+	# attr_accessor :name is equivalent to the code below
 	# attr_accessor adds 2 instance methods to our objects, a reader and a writer.
-	# Dog#name method for the reader, and the Dog#name= method for the writer. 
+	# Dog#name method for the reader, and the Dog#name= method for the writer.
 	def name=(name)
-		@name = name 
+		@name = name
 	end
 
 	def name
@@ -38,6 +38,6 @@ class Dog
 	end
 
 	def bark
-		puts “Woof!!! #{@the_name}”
+		puts "woof!" #{@the_name}”
 	end
 end
