04c2caaeed50b0746754c2bd3e5ee5fdb2c1f97f
Liebe Brand <liebebrand@gmail.com>
2018-08-22 14:59:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,17 +5,17 @@ class Dog
   def initialize(name) # initialize the method
     @name = name
   end
-  # 
+  #
   # def name=(name) # writing the method
   #   @name = name
   # end
-  # 
+  #
   # def name # reading the method
   #   @name
   # end
-  # 
+  #
   # def bark
 	# 	puts "Woof!"
 	# end
-  # 
+  #
 end
