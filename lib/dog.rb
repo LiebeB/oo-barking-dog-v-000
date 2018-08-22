d4c4fd5d55c2daa06062557e023fc39fd2000e8c
Liebe Brand <liebebrand@gmail.com>
2018-08-22 15:14:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,39 +1,15 @@
 # Your code goes here!
 
-# class Dog
-#
-#   def initialize(name) # initialize the method
-#     @name = name
-#   end
-#
-#   def name=(name) # writing the method
-#     @name = name
-#   end
-#
-#   def name # reading the method
-#     @name
-#   end
-#
-#   def bark
-# 		puts "woof!"
-# 	end
-#
-# end
-
-
-
-
-
 class Dog
-	def name=(name)
+	def name=(name) # writing the method
 		@name = name
 	end
 
-	def name
+	def name # reading the method
 		@name
 	end
 
 	def bark
-		puts "woof!" #{@the_name}”
+		puts "woof!" 
 	end
 end
